0f4f4c523470a7490f42b146d343244c9bdd8bd2
Yazid Louda <72033741+yazidlouda@users.noreply.github.com>
2020-11-12 17:14:38 -0500

    Revert "Ashley"

--- SocialMedia/Models/IdentityModels.cs
+++ SocialMedia/Models/IdentityModels.cs
@@ -1,7 +1,4 @@
-﻿using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration;
-using System.Data.Entity.ModelConfiguration.Conventions;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -20,20 +17,6 @@ namespace SocialMedia.Models
             return userIdentity;
         }
     }
-    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
-    {
-        public IdentityUserLoginConfiguration()
-        {
-            HasKey(iul => iul.UserId);
-        }
-    }
-    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
-    {
-        public IdentityUserRoleConfiguration()
-        {
-            HasKey(iur => iur.UserId);
-        }
-    }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
@@ -45,22 +28,6 @@ namespace SocialMedia.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
-        }
-        public DbSet<Post> Post { get; set; }
-        //public DbSet<Like> Like { get; set; }
-        //public DbSet<Comment> Comment { get; set; }
-        //public DbSet<Reply> Reply { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder
-                .Conventions
-                .Remove<PluralizingTableNameConvention>();
-
-            modelBuilder
-                .Configurations
-                .Add(new IdentityUserLoginConfiguration())
-                .Add(new IdentityUserRoleConfiguration());
         }
     }
 }
