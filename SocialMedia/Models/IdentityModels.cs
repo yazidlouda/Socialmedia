18295c09d14bccceb802f7710c0c3989d87be28e
yazidLouda <yalou1231@hotmail.com>
2020-11-12 17:01:07 -0500

    branch for testing

--- SocialMedia/Models/IdentityModels.cs
+++ SocialMedia/Models/IdentityModels.cs
@@ -1,9 +1,11 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
-
 namespace SocialMedia.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
@@ -17,6 +19,20 @@ namespace SocialMedia.Models
             return userIdentity;
         }
     }
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
+        }
+    }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
@@ -24,10 +40,26 @@ namespace SocialMedia.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-        
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+       // public DbSet<Post> Post { get; set; }
+       // public DbSet<Like> Like { get; set; }
+        //public DbSet<Comment> Comment { get; set; }
+        public DbSet<Reply> Reply { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
         }
     }
 }
---
+++ SocialMedia/Models/Reply.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SocialMedia.Models
+{
+    public class Reply
+    { 
+        [Key]
+        public int  Id { get; set; }
+        [Required]
+        public string ReplyComment { get; set; }
+    }
+}
