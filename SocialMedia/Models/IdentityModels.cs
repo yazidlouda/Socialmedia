1d85d231673a688a6376f85c75be14f6cddf4e84
JimGulley40 <jimgulley40@gmail.com>
2020-11-12 17:00:38 -0500

    comment crud

--- SocialMedia/Models/IdentityModels.cs
+++ SocialMedia/Models/IdentityModels.cs
@@ -1,8 +1,12 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+
 
 namespace SocialMedia.Models
 {
@@ -17,17 +21,49 @@ namespace SocialMedia.Models
             return userIdentity;
         }
     }
-
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
+        }
+    }
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-        
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
         }
+       // public DbSet<Post> Post { get; set; }
+        //public DbSet<Like> Like { get; set; }
+       public DbSet<Comment> Comment { get; set; }
+        //public DbSet<Reply> Reply { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
     }
 }
+
+
+
+
+
+
